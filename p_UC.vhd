adbe4a335ab55c24d8f01df4e5f6ddb4fdb3fcb0
froilan1987 <froilanba@utp.edu.co>
2017-04-15 02:29:03 -0500

    1

---
+++ p_UC.vhd
@@ -0,0 +1,94 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   02:27:57 04/15/2017
+-- Design Name:   
+-- Module Name:   E:/Usuario/Documentos/Xilinx/Procesador1.1/p_UC.vhd
+-- Project Name:  Procesador1.1
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: UC
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY p_UC IS
+END p_UC;
+ 
+ARCHITECTURE behavior OF p_UC IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT UC
+    PORT(
+         op : IN  std_logic_vector(1 downto 0);
+         op3 : IN  std_logic_vector(5 downto 0);
+         salida : OUT  std_logic_vector(5 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal op : std_logic_vector(1 downto 0) := (others => '0');
+   signal op3 : std_logic_vector(5 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal salida : std_logic_vector(5 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: UC PORT MAP (
+          op => op,
+          op3 => op3,
+          salida => salida
+        );
+
+   -- Clock process definitions
+   <clock>_process :process
+   begin
+		<clock> <= '0';
+		wait for <clock>_period/2;
+		<clock> <= '1';
+		wait for <clock>_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for <clock>_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
